0f99d6499f57bf39d9583b3c7e8364d4342637b3
nadavbh12 <nadavbh@gmail.com>
2015-12-23 11:17:23 +0200

    removed old snes9x which isn't a submodule

--- snes9x/gtk/src/filter_epx_unsafe.h
+++
@@ -1,7 +0,0 @@
-#ifndef __FILTER_EPX_UNSAFE_H
-#define __FILTER_EPX_UNSAFE_H
-
-void EPX_16_unsafe (uint8 *, uint32, uint8 *, uint32, int, int);
-void EPX_16_smooth_unsafe (uint8 *, uint32, uint8 *, uint32, int, int);
-
-#endif /* __FILTER_EPX_UNSAFE_H */
--- snes9x/gtk/src/filter_xbrz.h
+++
@@ -1,8 +0,0 @@
-#ifndef _filter_xbrz_h_
-#define _filter_xbrz_h_
-
-void filter_2xBRZ (uint8 *, uint32, uint8 *, uint32, int, int);
-void filter_3xBRZ (uint8 *, uint32, uint8 *, uint32, int, int);
-void filter_4xBRZ (uint8 *, uint32, uint8 *, uint32, int, int);
-
-#endif
