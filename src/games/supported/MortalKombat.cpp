4846b76d42f0d88d4fef9b43ec63846fccb4b373
shairoz <shai.roz1989@gmail.com>
2016-03-15 18:38:26 +0200

    changed MK character to Raiden

--- src/games/supported/MortalKombat.cpp
+++ src/games/supported/MortalKombat.cpp
@@ -166,6 +166,7 @@ ActionVect MortalKombatSettings::getStartingActions(){
 	int i, num_of_xs(4),num_of_nops(100);
 	ActionVect startingActions;
 	startingActions.reserve(num_of_xs*num_of_nops);
+
 	// wait for intro to end
 	for(i = 0; i<16*num_of_nops; i++){
 		startingActions.push_back(JOYPAD_NOOP);
@@ -173,13 +174,21 @@ ActionVect MortalKombatSettings::getStartingActions(){
 	// select tournament
 	startingActions.push_back(JOYPAD_X);
 	// wait for character select screen
-	for(i = 0; i<3*num_of_nops; i++){
+	for(i = 0; i<3.5*num_of_nops; i++){
 		startingActions.push_back(JOYPAD_NOOP);
 	}
-	// choose default character (kano)
+	// choose Raiden
+	startingActions.push_back(JOYPAD_DOWN);
+	startingActions.push_back(JOYPAD_DOWN);
+	for(i = 0; i<num_of_nops; i++){
+		startingActions.push_back(JOYPAD_NOOP);
+	}
+	// select character
 	startingActions.push_back(JOYPAD_X);
 	startingActions.push_back(JOYPAD_X);
-	startingActions.push_back(JOYPAD_X);
+	for(i = 0; i<num_of_nops; i++){
+		startingActions.push_back(JOYPAD_NOOP);
+	}
 	// wait for game to begin
 	for(i = 0; i<4*num_of_nops; i++){
 		startingActions.push_back(JOYPAD_NOOP);
