1e26a79f91687e4ffe74528db1f4552a66fe9261
Nadavbh <nadavbh@gmail.com>
2017-02-14 10:33:53 +0200

    added missing script from previous commit

---
+++ copy_cores.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp snes9x2010/*.so rle_python_interface/snes9x2010_libretro.so
+cp stella-libretro/*.so rle_python_interface/stella_libretro.so
+cp Genesis-Plus-GX/*.so rle_python_interface/genesis_plus_gx_libretro.so
+
